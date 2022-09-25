ee32223ba16ac3266d3f7cfc0ef2c1c8a4d45e50
tatsuya-asami <sharekobekko@gmail.com>
2022-09-25 22:10:23 +0900

    refactor: Not import component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,46 @@
 import type { NextPage } from "next";
-import { GridTemplate } from "../components/GridTemplate";
 
 const Home: NextPage = () => {
-  return <GridTemplate />;
+  return (
+    <div className="wrapper">
+      <div className="header">header</div>
+      <div className="left">left</div>
+      <div className="main">main</div>
+      <div className="right">right</div>
+      <div className="footer">footer</div>
+
+      <style jsx>{`
+        .wrapper {
+          height: 100vh;
+          display: grid;
+          grid-template:
+            "header header header"
+            "left   main   right"
+            "footer footer footer";
+        }
+        .header {
+          grid-area: header;
+          background-color: pink;
+        }
+        .left {
+          grid-area: left;
+          background-color: green;
+        }
+        .main {
+          grid-area: main;
+          background-color: yellow;
+        }
+        .right {
+          grid-area: right;
+          background-color: red;
+        }
+        .footer {
+          grid-area: footer;
+          background-color: aqua;
+        }
+      `}</style>
+    </div>
+  );
 };
 
 export default Home;

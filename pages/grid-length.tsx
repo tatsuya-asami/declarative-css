7becf332d8e990f10f2ca6bbca4a93ae0288ecd3
tatsuya-asami <sharekobekko@gmail.com>
2022-09-25 22:14:58 +0900

    feat: Add GridLength page

---
+++ pages/grid-length.tsx
@@ -0,0 +1,47 @@
+import type { NextPage } from "next";
+
+const GridLength: NextPage = () => {
+  return (
+    <div className="wrapper">
+      <div className="header">header</div>
+      <div className="left">left</div>
+      <div className="main">main</div>
+      <div className="right">right</div>
+      <div className="footer">footer</div>
+
+      <style jsx>{`
+        .wrapper {
+          height: 100vh;
+          display: grid;
+          grid-template:
+            "header header header" 100px
+            "left   main   right" 1fr
+            "footer footer footer" 300px
+            / 2fr 1fr 3fr;
+        }
+        .header {
+          grid-area: header;
+          background-color: pink;
+        }
+        .left {
+          grid-area: left;
+          background-color: green;
+        }
+        .main {
+          grid-area: main;
+          background-color: yellow;
+        }
+        .right {
+          grid-area: right;
+          background-color: red;
+        }
+        .footer {
+          grid-area: footer;
+          background-color: aqua;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default GridLength;

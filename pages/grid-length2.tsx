3aa9c8c1816bed8244cf280dea9e9396f59184ba
tatsuya-asami <sharekobekko@gmail.com>
2022-09-25 22:24:26 +0900

    feat: Add grid-length2

---
+++ pages/grid-length2.tsx
@@ -0,0 +1,60 @@
+import type { NextPage } from "next";
+
+const GridLength2: NextPage = () => {
+  return (
+    <div className="wrapper">
+      <div className="header">header</div>
+      <div className="left">left</div>
+      <div className="centerTop">centerTop</div>
+      <div className="centerMiddle">centerMiddle</div>
+      <div className="centerBottom">centerBottom</div>
+      <div className="right">right</div>
+      <div className="footer">footer</div>
+
+      <style jsx>{`
+        .wrapper {
+          height: 100vh;
+          display: grid;
+          grid-template:
+            "left header       header" 100px
+            "left centerTop    right" 1fr
+            "left centerMiddle right" 3fr
+            "left centerBottom right" 1fr
+            "left centerBottom footer" 300px
+            / 300px 1fr 3fr;
+          gap: 10px 20px;
+        }
+        .header {
+          grid-area: header;
+          background-color: pink;
+        }
+        .left {
+          grid-area: left;
+          background-color: green;
+        }
+        .centerTop {
+          grid-area: centerTop;
+          background-color: blue;
+        }
+        .centerMiddle {
+          grid-area: centerMiddle;
+          background-color: black;
+        }
+        .centerBottom {
+          grid-area: centerBottom;
+          background-color: white;
+        }
+        .right {
+          grid-area: right;
+          background-color: red;
+        }
+        .footer {
+          grid-area: footer;
+          background-color: aqua;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default GridLength2;
